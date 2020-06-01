110b0e441efb312fdabb4e2999982b4509f0f83f
balabhaskar <guntupalli.bala@searce.com>
2020-06-01 16:20:28 +0530

    varibles content was changed

--- variables.tf
+++ variables.tf
@@ -2,4 +2,8 @@ variable "vpc_name" {
   description = "give vpc name , which will create vpc on that name"
   type        = string
 }
+variable "cluster-name" {
+  description = "provide eks clsuer name"
+  type        = string  
+}
 
