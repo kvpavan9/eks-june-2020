93387827a1ca366235daa550774ae1be4fed3330
balabhaskar <guntupalli.bala@searce.com>
2020-06-03 12:41:36 +0530

    variables, terraform vars file was changed

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,2 @@
+cluster-name     = "eks-cluster-demo"
 node-group-names = ["node-group-1", "node-group-2"]
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "node-group-names" {
   description = "provide node group names"
   type        = list(string)
 }
+variable "cluster-name" {
+  default = "terraform-eks-demo"
+  type    = string
+}
+
