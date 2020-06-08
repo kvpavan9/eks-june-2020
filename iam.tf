7b4434f00d63a3c75c456be627d18ceeb98beb08
balabhaskar <guntupalli.bala@searce.com>
2020-06-08 19:36:55 +0530

    good code on june8

--- iam.tf
+++ iam.tf
@@ -16,30 +16,18 @@ resource "aws_iam_role" "eks_cluster" {
 }
 POLICY
 }
-
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster.name
 }
-
 resource "aws_iam_role_policy_attachment" "AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.eks_cluster.name
 }
 
-resource "aws_eks_cluster" "aws_eks" {
-  name     = "eks_cluster_tuto"
-  role_arn = aws_iam_role.eks_cluster.arn
+## eks-master Iam contetn completed
 
-  vpc_config {
-    subnet_ids = ["subnet-5721cb76", "subnet-5b5cb03d"]
-  }
-
-  tags = {
-    Name = "EKS_tuto"
-  }
-}
-
+## from eks node groups IAM contenst starts
 resource "aws_iam_role" "eks_nodes" {
   name = "eks-node-group-tuto"
 
@@ -58,7 +46,6 @@ resource "aws_iam_role" "eks_nodes" {
 }
 POLICY
 }
-
 resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.eks_nodes.name
@@ -74,24 +61,3 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   role       = aws_iam_role.eks_nodes.name
 }
 
-resource "aws_eks_node_group" "node" {
-  cluster_name    = aws_eks_cluster.aws_eks.name
-  count           = length(var.node-group-names)
-  node_group_name = var.node-group-names[count.index]
-  node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = ["subnet-5721cb76", "subnet-5b5cb03d"]
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 2
-  }
-
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-  ]
-}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,8 @@
-node-group-names = ["node-group-1", "node-group-2"]
+region           = "ap-southeast-1"
+cluster-name     = "eks-cluster-demo"
+node-group-names = ["node-group-1"]
+worker_EBS_volume_size        = 20
+worker_node_instance_type     = ["t2.medium"]
+worker_node_desired_instances = 2
+worker_node_max_instances     = 4
+worker_node_min_instances     = 2
